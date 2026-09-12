25fdc3f10c9760932b5205eee82bc70ac3d2e5d7
Ivan Malison <IvanMalison@gmail.com>
2026-09-12 15:23:57 -0700

    Add reproducible development and CI tooling

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "One Google Messages connection for all your devices";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "multiconnect-bridge";
+            version = "0.1.0-dev";
+            src = ./.;
+
+            go = pkgs.go_1_26;
+            vendorHash = "sha256-dBn+gREbDGlHJ7hNmvw9zhhzg3AX2s17hcbtPvH0iHI=";
+
+            meta = {
+              description = "One Google Messages connection for all your devices";
+              homepage = "https://github.com/colonelpanic8/multiconnect-bridge";
+              license = pkgs.lib.licenses.agpl3Plus;
+              mainProgram = "multiconnect-bridge";
+              platforms = supportedSystems;
+            };
+          };
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = nixpkgs.lib.getExe self.packages.${system}.default;
+          meta.description = "Run Multiconnect Bridge";
+        };
+      });
+
+      checks = forAllSystems (system: {
+        package = self.packages.${system}.default;
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              actionlint
+              go_1_26
+              go-tools
+              gopls
+              just
+              nixfmt
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+
+      overlays.default = final: _prev: {
+        multiconnect-bridge = self.packages.${final.stdenv.hostPlatform.system}.default;
+      };
+    };
+}
---
+++ justfile
@@ -0,0 +1,20 @@
+fmt:
+    gofmt -w cmd internal
+    nixfmt flake.nix
+
+fmt-check:
+    test -z "$(gofmt -l cmd internal)"
+    nixfmt --check flake.nix
+
+lint:
+    go vet ./cmd/... ./internal/...
+    staticcheck ./...
+    actionlint
+
+test:
+    go test -race ./...
+
+check: fmt-check lint test
+
+build:
+    go build -o bin/multiconnect-bridge ./cmd/multiconnect-bridge
